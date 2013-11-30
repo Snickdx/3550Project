42d364f6a89d03c3131931863ef5aab0efa933c8
djskeg <stefan-hosein@hotmail.com>
2013-11-30 11:27:22 -0800

    changed js file to be more modular

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,8 +4,8 @@
         <script src="<?php echo base_url();?>js/vendor/jquery-1.10.1.min.js"></script>
         <script src="<?php echo base_url();?>js/highcharts.js"></script>
         <script src="<?php echo base_url();?>js/vendor/bootstrap.min.js"></script>
-
         <script src="<?php echo base_url();?>js/main.js"></script>
+        <script src="<?php echo base_url();?>js/mem1.js"></script>
     </body>
 </html>
 
